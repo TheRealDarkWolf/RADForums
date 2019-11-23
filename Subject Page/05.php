a7dfb2d67307d182c0c8e2222a031062aff4256d
krishnas2k <krishnas2k@gmail.com>
2019-11-23 06:31:21 +0530

    Adding jQuery to home and subject pages

--- Subject Page/05.php
+++ Subject Page/05.php
@@ -10,15 +10,15 @@
         <link rel="icon" href="../images/logotitle.png" type="image/x-icon">
     </head>
         <?php require('../templates/header.php'); ?>
-            <h1 id="subject-title">Digital Discrete Mathematics and Logic-UE18CS205</h1>
+            <h1 id="subject-title">Discrete Mathematics and Logic-UE18CS205</h1>
             <div class="container">
                 <div class="left-container">
                     <div id="info-block">
-                        <h3>IMPORTANT DATES</h3>
-                        <p>ISA-1:</p>
-                        <p>ISA-2:</p>
-                        <p>ESA:</p>
-                        <p>ASSIGNMENT WEEK:</p>
+                    <h3>IMPORTANT DATES</h3>
+                        <p>ISA-1: Completed in September</p>
+                        <p>ISA-2: Completed on 8 November</p>
+                        <p>ESA: To begin on 27 November</p>
+                        <p>ASSIGNMENT WEEK: 17 November to 23 November</p>
                     </div>
                     <div class="thread uploads">
                         <h3>LATEST UPLOADS IN UE18CS205</h3>
@@ -74,7 +74,7 @@
                         <h3>SUBJECT DETAILS</h3>
                         <p>COURSE CODE: UE18CS205</p>
                         <p>SEMESTER: 3</p>
-                        <p>ANCHOR: ----</p>
+                        <p>ANCHOR: Sangeeta V I</p>
                     </div>
                     
                     <div id="new-thread">
@@ -102,9 +102,9 @@
                         <button>Download Syllabus for Next Semester</button>
                     </div>
                     <?php require('../templates/footer.php'); ?>
-               <script src="subject.js"></script>
-        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+       <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+        <script src="subject.js"></script>
     </body>
 </html>
