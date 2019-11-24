4e74f9aa652b0455d25032c08128ffa84587e5b1
Dark Wolf <43084105+TheRealDarkWolf@users.noreply.github.com>
2019-11-24 17:38:46 +0530

    Update db_connect.php

--- templates/db_connect.php
+++ templates/db_connect.php
@@ -1,6 +1,6 @@
 <?php 
   //connect to a database
-  $conn = mysqli_connect('localhost','darkwolf','dmewolverine4114','radforums');
+  $conn = mysqli_connect('localhost','darkwolf','','radforums');
   if(!$conn){
       echo 'Connection error: ' . mysqli_connect_error();
   }
